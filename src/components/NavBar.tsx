628b0ca07e85beefde235a5ad9cf210be908e149
OctavioBenjamin <benjaminmen55@gmail.com>
2024-07-11 12:26:46 -0300

    add Links

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,21 +1,21 @@
 import "./NavBar.css"
-
+import { Link } from "react-router-dom";
 function NavBar() {
     return (
         <>
             <div className="navbar-container">
                 <ul className="navbar">
                     <li className="nav-item">
-                        {/* <Link className="nav-link" to="/">Characters</Link> */}
-                        <a href="/" className="nav-link">Characters</a>
+                        <Link className="nav-link" to="/">Characters</Link>
+                        {/* <a href="/" className="nav-link">Characters</a> */}
                     </li>
                     <li className="nav-item">
-                        {/* <Link  className="nav-link"to="/locations">Locations</Link> */}
-                        <a href="/rickandmorty_api/locations" className="nav-link">Locations</a>
+                        <Link  className="nav-link" to="/locations">Locations</Link>
+                        {/* <a href="/locations" className="nav-link">Locations</a> */}
                     </li>
                     <li className="nav-item">
-                        {/* <Link  className="nav-link"to="/episodes">Episodes</Link> */}
-                        <a href="/rickandmorty_api/episodes" className="nav-link">Episodes</a>
+                        <Link  className="nav-link"to="/episodes">Episodes</Link>
+                        {/* <a href="/episodes" className="nav-link">Episodes</a> */}
                     </li>
                 </ul>
             </div>
